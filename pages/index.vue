a731c6e17089e20c52618ee890ab022b49a59e59
leo <le4ndro.fioretti@gmail.com>
2025-03-10 11:34:59 +0100

    d

--- pages/index.vue
+++ pages/index.vue
@@ -39,8 +39,35 @@ const data = [
 </script>
 
 <template>
-  <div>
-    <AreaChart :data="data" index="name" :categories="['total', 'predicted']" />
+  <div class="size-full">
+    <AreaChart
+      :data="data"
+      index="name"
+      :categories="['total', 'predicted']"
+      class="!max-w-[50%]"
+    />
+    <div class="w-full grid grid-cols-2 gap-4">
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent> Card Content </CardContent>
+          <CardFooter> Card Footer </CardFooter>
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent> Card Content </CardContent>
+          <CardFooter> Card Footer </CardFooter>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
 
